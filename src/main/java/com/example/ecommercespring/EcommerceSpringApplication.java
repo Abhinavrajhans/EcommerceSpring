2f07aeb5f9e34458bd1296fb75a4fb33524b782b
Abhinav Rajhans <abhinav.rajhans@qodeinvest.com>
2025-06-28 16:45:46 +0530

    added git env

--- src/main/java/com/example/ecommercespring/EcommerceSpringApplication.java
+++ src/main/java/com/example/ecommercespring/EcommerceSpringApplication.java
@@ -2,12 +2,16 @@ package com.example.ecommercespring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class EcommerceSpringApplication {
 
     public static void main(String[] args) {
-
+        Dotenv dotenv=Dotenv.configure().load();
+        dotenv.entries().forEach((entry)->{
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
         SpringApplication.run(EcommerceSpringApplication.class, args);
     }
 
